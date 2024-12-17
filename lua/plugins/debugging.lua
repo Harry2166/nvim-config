2e2ac4101161e9f9e396d1cd08c7910f775202db
Harry2166 <harry2166.hq@gmail.com>
2024-12-17 23:09:24 +0800

    feat: add dap python (it doesn't even work)

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,11 +1,15 @@
 return {
   'mfussenegger/nvim-dap',
   dependencies = {
-    "rcarriga/nvim-dap-ui"
+    "rcarriga/nvim-dap-ui",
+    "mfussenegger/nvim-dap-python"
   },
   config = function()
   local dap = require("dap")
   local dapui = require("dapui")
+  local dap_python = require("dap-python")
+  dap_python.setup("python")
+  dap_python.test_runner = 'pytest'
   dap.listeners.before.attach.dapui_config = function()
     dapui.open()
   end
@@ -18,7 +22,7 @@ return {
   dap.listeners.before.event_exited.dapui_config = function()
     dapui.close()
   end
-      vim.keymap.set('n', '<Leader>br', dap.toggle_breakpoint, {})
+      vim.keymap.set('n', '<Leader>dt', dap.toggle_breakpoint, {})
     vim.keymap.set('n', '<Leader>dc', dap.continue, {})
   end,
 }
